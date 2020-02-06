b46bc83ed5e4a1ff64cdb4ef610b643c4d908605
Alberto <acama056@fiu.edu>
2020-02-06 01:06:32 -0500

    DateFormatter with new Post compoent, still need pageContent

---
+++ Sources/Blog/AlbertoTheme/Nodes/Posts.swift
@@ -0,0 +1,36 @@
+//
+//  Node+Post.swift
+//
+//
+//  Created by Povilas Staskus on 1/26/20.
+//
+
+import Foundation
+import Plot
+import Publish
+
+extension Node where Context == HTML.BodyContext {
+    static func post(for item: Item<Blog>, on site: Blog) -> Node {
+        return .pageContent(
+            .h2(
+                .class("post-title"),
+                .a(
+                    .href(item.path),
+                    .text(item.title)
+                )
+            ),
+            .p(
+                .class("post-meta"),
+                .text(DateFormatter.blog.string(from: item.date))
+            ),
+            .tagList(for: item, on: site),
+            .div(
+                .class("post-description"),
+                .div(
+                    .contentBody(item.body)
+                )
+            )
+        )
+    }
+}
+
